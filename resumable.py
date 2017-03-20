92bc2218aaa3dc61c8534c6f09485fedb298e8c7
Dominic May <jack.thatch@gmail.com>
2017-03-20 23:24:30 +0800

    Actually support `value()`

--- resumable.py
+++ resumable.py
@@ -10,6 +10,7 @@ from astmonkey.visitors import SourceGeneratorNodeVisitor
 
 
 SPLIT = 'split'
+VALUE = 'value'
 
 
 def split(func, name=None):
@@ -93,11 +94,14 @@ class Visitor(ast.NodeVisitor):
         return func
 
     def visit_Call(self, node):
-        if getattr(node.func, 'id', None) == SPLIT:
+        if getattr(node.func, 'id', None) in {SPLIT, VALUE}:
+            is_value = node.func.id == VALUE
+
             # this is the expression that contains the call,
             # or basically the value of the assignment/return
-            expr = node.parent
-            user = expr.parent  # by user we mean user of the value
+            user = node.parent
+            if not is_value:
+                user = user.parent  # by user we mean user of the value
 
             # it's possible more are actually supported,
             # but i'm hesitant to just allow them without
@@ -113,7 +117,10 @@ class Visitor(ast.NodeVisitor):
             body = field[self.last_idx + 1:user.parent_field_index]
 
             value = user.value
-            value.func = value.func.args[0]  # remove call to split
+            if is_value:
+                value = value.args[0]
+            else:
+                value.func = value.func.args[0]  # remove call to split
             body.append(ast.Return(value))
             body[-1].lineno = value.lineno
 
